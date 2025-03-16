06b535b43d1eb953e26999efbe887c48e2ce7f54
Vladislav Galagan <vlad081210@gmail.com>
2025-03-16 22:29:16 +0200

    Homework2

---
+++ main.js
@@ -0,0 +1,21 @@
+"use strict";
+// const a = prompt ("Як Вас звати?");
+// const b = prompt ("Скільки Вам років?");
+// const c = prompt ("Звідки Ви?");
+
+// console.log(
+//    `
+//    ${a}
+//    ${b}
+//    ${c}`);
+
+const a = [
+   prompt ("Як Вас звати?"),
+   prompt ("Скільки Вам років?"),
+   prompt ("Звідки Ви?"),
+];
+
+console.log(`
+   1. ${a [0]}
+   2. ${a[1]}
+   3. ${a[2]}`)
